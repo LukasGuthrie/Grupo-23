98356532f9fb93271ec85b64cfb8ce6477097f43
LukasGuthrie <lguthrie@uc.cl>
2023-10-25 22:11:25 -0300

    Terminadas las restricciones

--- main.py
+++ main.py
@@ -29,28 +29,29 @@ model.addConstrs(cv[p,z,0] == 0 for p in Puntos_Dispo for z in Zonas)
 model.addConstrs(quicksum(cv[p,z,d] for p in Puntos_Dispo for z in Zonas) == quicksum(cv[p,z,(d-1)] for p in Puntos_Dispo for z in Zonas) + quicksum(demanda_diaria_volumen_por_zona_dia[z,d] for z in Zonas) - quicksum(ev[t,z,d] for t in Puntos_Extra for z in Zonas) - quicksum(rv[a,d] for a in Camiones) for d in range(1,7))
 
 #3 Se satisface la demanda de requerimiento de reciclaje por zona.
-model.addConstrs()
+model.addConstrs(quicksum(demanda_diaria_volumen_por_zona_dia[z,d] for z in Zonas) <= quicksum(cv[p,z,d] for p in Puntos_Dispo) + quicksum(ev[t,z,d] for t in Puntos_Extra) for z in Zonas for d in Dias)
 
 #4 Los puntos de reciclaje extra anadidos siguen los requerimientos de que el area que abarca el punto de reciclaje debe ser respetado.
-model.addConstrs()
+model.addConstrs(ev[t,z,d] <= volumen_max_por_punto_extra for t in Puntos_Extra for z in Zonas for d in Dias)
 
 #5 La cantidad de puntos de reciclaje extra se realiza segun la cantidad de material necesario extra recibido.
-model.addConstrs()
+model.addConstrs(quicksum(ev[t,z,d] for t in Puntos_Extra) <= (volumen_max_por_punto_extra * e[z,d]) for z in Zonas for d in Dias)
 
 #6 La seleccion del numero de camiones se realiza segun la cantidad de material necesario para retirar.
-model.addConstrs()
+model.addConstrs(quicksum(rv[a,d] for a in Camiones) <= (volumen_max_camion * rn[d]) for d in Dias)
 
 #7 Los camiones no pueden superar su capacidad maxima de reciclaje.
-model.addConstrs()
+model.addConstrs(rv[a,d] <= volumen_max_camion for a in Camiones for d in Dias)
 
 #8 Los camiones no pueden superar el numero de camiones disponibles.
-model.addConstrs()
+model.addConstrs(rn[d] <= len(Camiones) for d in Dias)
 
 #9 Los puntos de reciclaje extra no pueden superar el n´umero de puntos extra disponibles para el despliegue.
-model.addConstrs()
+model.addConstrs(quicksum(e[z,d] for z in Zonas) for d in Dias)
 
 #10 No se debe sobrepasar el presupuesto asignado por la municipalidad para los puntos de reciclaje extra.
-model.addConstrs()
+model.addConstrs(quicksum(costo_utilizacion_punto_extra * e[z,d] for z in Zonas for d in Dias) <= presupuesto_municipalidad_puntos_extra)
+model.addConstrs(quicksum(costo_utilizacion_punto_extra * e[z,d] for z in Zonas for d in Dias) >= 0)
 
 
 model.optimize()
